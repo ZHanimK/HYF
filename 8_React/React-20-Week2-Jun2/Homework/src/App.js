463219493768fbd32747b8e930a3fffc3bbc59d2
ZHanimK <zilhan.be@gmail.com>
2019-06-06 19:40:40 +0200

    debugging

--- 8_React/React-20-Week2-Jun2/Homework/src/App.js
+++ 8_React/React-20-Week2-Jun2/Homework/src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDoItem from './toDoItem'
+import AddItem from './addItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todosList=[
+  {
+    title: "Buy the groceries",
+    isChecked:false
+  },
+  {
+    title: "do homework",
+    isChecked:true
+  },
+  {
+    title: "do the laundary",
+    isChecked:false
+  }
+]
+
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state={
+      todos: todosList
+    }
+  }
+
+  addToDoItem=(title)=>{
+    const newToDo={
+      title: title,
+      isChecked: false
+    }
+    this.setState({todos:[...this.state.todos, newToDo]})
+    
+  }
+  render() {
+    return (
+      <div>
+        <AddItem addToItemCallback={this.addToDoItem}/>
+        <ul>{
+          this.state.todos.map(todoItem=>{
+          return <ToDoItem title={todoItem.title}/>
+        })
+        }</ul>
+      </div>
+    )
+  }
 }
 
 export default App;
