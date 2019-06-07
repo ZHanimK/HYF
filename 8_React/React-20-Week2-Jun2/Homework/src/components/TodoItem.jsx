1dd60ee0affbe79001b0933f1a3dac49772f5c02
ZHanimK <zilhan.be@gmail.com>
2019-06-07 15:53:41 +0200

    done-undone

--- 8_React/React-20-Week2-Jun2/Homework/src/components/TodoItem.jsx
+++ 8_React/React-20-Week2-Jun2/Homework/src/components/TodoItem.jsx
@@ -2,6 +2,21 @@ import React from "react";
 
 
 export function TodoItem(props){
+    // return (
+    //     <div style={{ display: "flex", justifyContent: "left" }}>
+    //     <div style={{content: props.done ? "Undone" : "Done"}} onClick={props.toggleDone}>Undone</div>
+    //     <div
+    //       style={{
+    //         textDecoration: props.done ? "line-through" : ""
+    //       }}
+    //       onClick={props.toggleDone}
+    //     >
+    //       {props.description}
+    //     </div>
+    //     <button onClick={props.deleteTodo }>X</button>
+    //   </div>
+    
+    // );
     if (props.done===false){
       return (
         <div style={{ display: "flex", justifyContent: "left" }}>
