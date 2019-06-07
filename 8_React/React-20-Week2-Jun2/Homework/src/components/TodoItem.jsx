65381b78bb453fe2fb3f91aa66b3b35188ad509b
ZHanimK <zilhan.be@gmail.com>
2019-06-07 16:15:36 +0200

    debugging

--- 8_React/React-20-Week2-Jun2/Homework/src/components/TodoItem.jsx
+++ 8_React/React-20-Week2-Jun2/Homework/src/components/TodoItem.jsx
@@ -20,39 +20,5 @@ export function TodoItem(props){
       </div>
     
     );
-    // if (props.done===false){
-    //   return (
-    //     <div style={{ display: "flex", justifyContent: "left" }}>
-    //     <div style={{color: "red"}} onClick={props.toggleDone}>Undone &nbsp;</div>
-    //     <div
-    //       style={{
-    //         textDecoration: props.done ? "line-through" : ""
-    //       }}
-    //       onClick={props.toggleDone}
-    //     >
-    //       {props.description}
-    //     </div>
-    //     <button onClick={props.deleteTodo }>X</button>
-    //   </div>
     
-    // );
-    // } else {
-    //   return (
-    //     <div style={{ display: "flex", justifyContent: "left" }}>
-    //     <div style={{color: "green"}} onClick={props.toggleDone}>Done &nbsp;</div>
-    //     <div
-    //       style={{
-    //         textDecoration: props.done ? "line-through" : ""
-    //       }}
-    //       onClick={props.toggleDone}
-    //     >
-    //       {props.description}
-    //     </div>
-    //     <button onClick={props.deleteTodo }>X</button>
-    //   </div>
-    
-    // );
-    // }
-  
-
 }
