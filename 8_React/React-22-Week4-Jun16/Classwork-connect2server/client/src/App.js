0b12b81cbd7f5e6b2bc88a2080799c9bacabc292
ZHanimK <zilhan.be@gmail.com>
2019-06-16 13:43:22 +0200

    classwork-connect-to-server

---
+++ 8_React/React-22-Week4-Jun16/Classwork-connect2server/client/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+    state = {
+        response: "",
+        post: "",
+        responseToPost: ""
+    };
+
+    handlePost = async () => {
+        const response = await fetch("/api/world", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ post: this.state.post })
+        });
+        const body = await response.text();
+
+        this.setState({ responseToPost: body });
+    };
+
+    handleGet = async () => {
+        const response = await fetch("/api/hello");
+        const body = await response.json();
+        console.log(body)
+        this.setState({ response: body.express });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.handleGet}>Get Request</button>
+                <p>Get answer: {this.state.response}</p>
+                <input
+                    type="text"
+                    value={this.state.post}
+                    onChange={e => this.setState({ post: e.target.value })}
+                />
+                <button onClick={this.handlePost}>Post Request</button>
+                <p>Post answer: {this.state.responseToPost}</p>
+            </div>
+        );
+    }
+}
+
+export default App;
